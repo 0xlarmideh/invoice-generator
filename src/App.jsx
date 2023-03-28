741b13070f87cd43dd242f6d988f732ee9c23691
0xlarmideh <busarioyewole@gmail.com>
2023-03-28 17:43:39 +0100

    FIX/Removed Edit page in routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Downloaded from "./views/Downloaded";
-import EditPage from "./EditPage";
 import HomePage from "./views/HomePage";
 // import InvoicingForm from "./views/InvoicingForm";
 import InvoicingForm from "./views/InvoicingForm";
@@ -14,9 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/invoice" element={<InvoicingFormRef />}></Route>
-        <Route path="/edit" element={<EditPage />}></Route>
         <Route path="/downloaded" element={<Downloaded />}></Route>
-
         <Route path="/preview" element={<PreviewPage />}></Route>
       </Routes>
     </BrowserRouter>
