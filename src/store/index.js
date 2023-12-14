976a242749be52782ebf31fdab3d4e0a2a076cb0
Abdulhameed <busarioyewole@gmail.com>
2023-12-14 20:58:22 +0100

    Added and configured Redux store for project

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit'
-import formReducer from "./slices/formSlice"
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-import logger from 'redux-logger'
+import { configureStore } from "@reduxjs/toolkit";
+import formReducer from "./slices/formSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-}
+};
 
-const persistedReducer = persistReducer(persistConfig, formReducer)
+const persistedReducer = persistReducer(persistConfig, formReducer);
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}).concat(logger),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }).concat(logger),
 });
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
 
-export default store
+export default store;
